9a56fca8cee12dcda74b3a34359c6cb837a76a48
Michael Muchiri <muchirimichael2222@gmail.com>
2023-09-30 12:01:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+function findMatching(array,string){
+    let newArray=[]
+    for(let item of array){
+        if(item.toLowerCase()===string.toLowerCase()){
+           newArray.push(item)
+        }
+    }
+    return newArray
+}
+
+function fuzzyMatch(array,string){
+    let newArray=[]
+    for(let item of array){
+        if(item.substring(0,1)===string.substring(0,1)){
+            newArray.push(item)
+        }
+    }
+    return newArray
+}
+
+function matchName(array,string){
+    let newArray=[]
+    for(let item of array){
+        if(item.name===string){
+            newArray.push(item)
+        }
+    }
+    return newArray
+}
